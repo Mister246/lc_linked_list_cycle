3b7d1495df0e3e0b674d09b162684cb09e9f7d68
Joel Elrod <joelelrod246@gmail.com>
2023-07-01 11:00:45 -0400

    Initial Commit
    Signed-off-by: Joel Elrod <joelelrod246@gmail.com>

---
+++ lc_linked_list_cycle/Program.cs
@@ -0,0 +1,19 @@
+﻿
+public class ListNode 
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x) 
+    {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution
+{
+    static public bool HasCycle(ListNode head)
+    {
+
+    }
+}
