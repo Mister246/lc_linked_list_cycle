a8306027027095b88de79a1162edc696eb315b11
Joel Elrod <joelelrod246@gmail.com>
2023-07-01 11:10:55 -0400

    First working version
    Signed-off-by: Joel Elrod <joelelrod246@gmail.com>

--- lc_linked_list_cycle/Program.cs
+++ lc_linked_list_cycle/Program.cs
@@ -14,6 +14,16 @@ public class Solution
 {
     static public bool HasCycle(ListNode head)
     {
-
+        List<ListNode> nodes = new List<ListNode>();
+        while (head != null)
+        {
+            if (nodes.Contains(head))
+            {
+                return true;
+            }
+            nodes.Add(head);
+            head = head.next;
+        }
+        return false;
     }
 }
